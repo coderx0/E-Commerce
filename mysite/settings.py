5d154ded9627a500d9ac6c52ea94ae6e1e2696a9
Sumeet Debnath <sumeetdeb100@gmail.com>
2021-06-09 13:50:28 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,8 +141,8 @@ MESSAGE_TAGS = {
 #smtp config
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = # an email account
-EMAIL_HOST_PASSWORD = #password
+EMAIL_HOST_USER = 'developer01sumeet@gmail.com'
+EMAIL_HOST_PASSWORD = 'legendcoder@001'
 EMAIL_USE_TLS = True
 
 
